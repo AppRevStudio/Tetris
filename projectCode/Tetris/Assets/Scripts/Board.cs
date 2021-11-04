4ee3dbaf162264994f00843dfd671e61190880cf
SergeiBak <77221025+SergeiBak@users.noreply.github.com>
2021-11-03 17:50:58 -0700

    TetrominoMovement

--- projectCode/Tetris/Assets/Scripts/Board.cs
+++ projectCode/Tetris/Assets/Scripts/Board.cs
@@ -7,6 +7,16 @@ public class Board : MonoBehaviour
     public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominos;
     public Vector3Int spawnPos;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2); // bottom left corner
+            return new RectInt(position, this.boardSize);
+        }
+    }
 
     private void Awake()
     {
@@ -37,6 +47,7 @@ public class Board : MonoBehaviour
         Set(this.activePiece);
     }
 
+    // render piece onto board
     public void Set(Piece piece)
     {
         for (int i = 0; i < piece.cells.Length; i++)
@@ -44,5 +55,40 @@ public class Board : MonoBehaviour
             Vector3Int tilePos = piece.cells[i] + piece.position;
             this.tilemap.SetTile(tilePos, piece.data.tile);
         }
+    }
+
+    // clear piece from board
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePos = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePos, null);
+        }
+    }
+
+    // checks if piece can move to new position, true if yes, false if no
+    public bool isValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+
+        for(int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePos = piece.cells[i] + position;
+
+            // check if tile is in bounds
+            if (!bounds.Contains((Vector2Int)tilePos))
+            {
+                return false;
+            }
+
+            // check if tile is already filled
+            if (this.tilemap.HasTile(tilePos))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- projectCode/Tetris/Assets/Scripts/Piece.cs
+++ projectCode/Tetris/Assets/Scripts/Piece.cs
@@ -26,4 +26,58 @@ public class Piece : MonoBehaviour
             this.cells[i] = (Vector3Int)data.cells[i];
         }
     }
+
+    private void Update()
+    {
+        this.board.Clear(this);
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(Vector2Int.left);
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(Vector2Int.right);
+        }
+
+        // Soft drop
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(Vector2Int.down);
+        }
+
+        // Hard drop
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }
+
+        this.board.Set(this);
+    }
+
+    // keeps moving down until can't move down any more
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+    }
+
+    // checks if position to move to is valid, if yes then move piece in that direction
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPos = this.position;
+        newPos.x += translation.x;
+        newPos.y += translation.y;
+
+        bool valid = this.board.isValidPosition(this, newPos);
+
+        if (valid)
+        {
+            this.position = newPos;
+        }
+
+        return valid;
+    }
 }
