c49ce85735ace96761960ca261b97fe4eb3116b4
SergeiBak <77221025+SergeiBak@users.noreply.github.com>
2021-11-03 19:11:41 -0700

    WallKicks

--- projectCode/Tetris/Assets/Scripts/Piece.cs
+++ projectCode/Tetris/Assets/Scripts/Piece.cs
@@ -94,15 +94,28 @@ public class Piece : MonoBehaviour
     // rotates piece left or right
     private void Rotate(int direction)
     {
+        int originalRotation = this.rotationIndex;
         this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4); // index bounds = 0, 1, 2, 3
 
+        ApplyRotationMatrix(direction);
+
+        // if fails wall kick test, reverse rotation
+        if (!TestWallKicks(this.rotationIndex, direction))
+        {
+            this.rotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+
+    private void ApplyRotationMatrix(int direction)
+    {
         for (int i = 0; i < this.cells.Length; i++)
         {
             Vector3 cell = this.cells[i];
 
             int x, y;
 
-            switch(this.data.tetromino)
+            switch (this.data.tetromino)
             {
                 case Tetromino.I:
                 case Tetromino.O:
@@ -120,6 +133,35 @@ public class Piece : MonoBehaviour
 
             this.cells[i] = new Vector3Int(x, y, 0);
         }
+    }
+
+    private bool TestWallKicks(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);
+
+        for (int i = 0; i < this.data.wallKicks.GetLength(1); i++)
+        {
+            Vector2Int translation = this.data.wallKicks[wallKickIndex, i];
+
+            if (Move(translation))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int GetWallKickIndex(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = rotationIndex * 2;
+
+        if (rotationDirection < 0)
+        {
+            wallKickIndex--;
+        }
+
+        return Wrap(wallKickIndex, 0, this.data.wallKicks.GetLength(0));
     }
 
     private int Wrap(int input, int min, int max)
--- projectCode/Tetris/Assets/Scripts/Tetromino.cs
+++ projectCode/Tetris/Assets/Scripts/Tetromino.cs
@@ -19,10 +19,12 @@ public struct TetrominoData
     public Tetromino tetromino;
     public Tile tile;
     public Vector2Int[] cells { get; private set; }
+    public Vector2Int[,] wallKicks { get; private set; }
 
     // sets the cells of the tetromino shape by fetching them from dictionary
     public void Initialize()
     {
         this.cells = Data.Cells[this.tetromino];
+        this.wallKicks = Data.WallKicks[this.tetromino];
     }
 }
